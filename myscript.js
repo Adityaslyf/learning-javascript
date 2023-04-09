9c7c74dcf0f5c71bf1f7109468897c1143808838
Aditya Varshney <aditya.varshneymail@gmail.com>
2023-04-09 23:51:05 +0530

    basics

---
+++ myscript.js
@@ -0,0 +1,257 @@
+
+
+//var x = "addy nagar";
+/*x = -69.2;
+x = true;
+ document.write(x);
+ document.write("<br>");
+ document.write(typeof x); */
+// var a = 69;
+//var b = 59;
+//a--;
+/*document.write(a+b);
+a++;
+document.write("<br>");
+document.write(25+22);
+a+=b;
+document.write(a)
+console.table([1,2,3,4]);
+console.error("something went wrong");
+console.time("Test");
+console.warn("this is warn"); 
+console.warn("this is warn");
+console.warn("this is warn");
+console.warn("this is warn");
+console.clear();
+console.timeEnd("Test");*/
+
+//console.log(a===b)
+/*if(b>60){
+   document.write("b is greater then 50");
+}*/
+
+/*if(a>=60 && a<=80){
+console.log("working");
+}*/
+/*if(a>=70){
+    document.write("it is correct")
+}else{
+    document.write("it is true")
+}*/
+
+/*(a<=60){
+    document.write("hello mr." + aditya);
+
+}else{
+    document.write("hello miss" + aditya);
+}*/
+/*if(b == 59){
+document.write("hello mr" + aditya);
+
+}else{
+    document.write("hello miss" + aditya);
+}*/
+
+/*var per =185;
+if(per >= 80 && per <=100){
+
+    document.write("you are tomper");
+
+}else if(per >=60 && per <80 ){
+    document.write("you are average");
+}else if (per >=33 && per<60){
+    document.write("you are below average");
+}else{ 
+    document.write("please enter valid deatisl");
+}*/
+
+/*var a =64;
+var b;
+b = "value is " + (a == 64 ? "True" : "False");
+document.write(b);*/
+
+
+// var day = 0;
+
+// switch (day) {
+
+//     case 0:
+//         document.write("today is monday");
+//         break;
+
+
+
+//     case 1:
+//         document.write("today is tuesday");
+//        break;
+
+//     case 2:
+//         document.write("today is wednesday");
+//         break;
+
+//     default:
+//         document.write("enter the day");
+// }
+// var a =7;
+// var b =9;
+// if (a>b){
+// alert("hello world");
+// }
+// else{
+//     alert("hello aditya");
+// }
+
+
+/*var a = confirm("do you love me?");
+
+
+if(a){
+
+    alert("thanks");
+
+}else{
+    alert("sorry");
+}*/
+/*var a = prompt("enter the details:");
+document.write(a);*/
+
+/*function hello 
+(){
+document.write("hello india");
+}
+
+hello();
+function yahoo(){
+    document.write("hello ji");
+}
+document.write("<br>");
+hello();
+yahoo();
+hello();*/
+
+/*function hello(fname, lname){
+    document.write("Hello" +fname + " " +lname);
+}
+hello("ram", "shyam");*/
+
+/*function sum (a,b){
+
+    document.write(a+b);
+}
+sum(20,30);*/
+
+/*function fullname(fname= "yahoo", lname= "baba")
+{
+var a = fname +"" +lame;
+return a;}
+var fn = fullname("ram","singh");
+document.write(fn);*/
+
+/*function sum (math,eng,hindi){
+    var s = math + eng + hindi;
+    return s;
+   
+}
+function percentage(tt){
+    var per = tt/300*100;
+    document.write(per);
+}
+var  total = sum (55,65,76);
+percentage(total);*/
+
+/*var a ="yahoo baba";
+  function hello(){
+
+ 
+    document.write(a +"<br>");
+  } hello();
+  document.write(a)*/
+
+/*function hello(){
+  alert("hello world");
+}*/
+/*var a= 1;
+while(a<=10){
+ document.write(a+".yahoo baaba<br>");
+ a = a+1;
+}*/
+
+/*var a= 1;
+document.write("<ul>")
+while(a<=10){
+ document.write("<li>yahoo baaba");
+ a = a+1;
+}
+ document.write("<ul>");*/
+
+/* var a = 1;
+ do {
+document.write("hello yahoo baba<br>");
+a =a+1;
+
+ }while(a<=10)*/
+
+/* for(var a=1; a<=10; a++){
+
+     document.write("hello world<br>");
+ }*/
+/*for (var a=1; a<=10; a++){
+    if (a==3){
+        document.write("hey:"+ a +"<br>");
+    }document.write("no."+a+"<br>");
+}*/
+
+
+// for (var a=1; a<=10; a++){
+//     if (a==3){
+//         document.write("hey:"+ a +"<br>");
+//         continue;
+//     }document.write("no."+a+"<br>");
+
+// }
+
+
+
+// for (var a=1; a<=10; a++){
+//     if (a==3){
+//         document.write("hey:"+ a +"<br>");
+//         break;
+//     }document.write("no."+a+"<br>");
+
+// }
+
+
+// for (var a = 1; a <= 10; a++) {
+//     if (a % 2 == 0) {
+// document.write(a+"<br>");
+//     }
+// }
+
+
+// for (var a = 1; a <= 10; a++) {
+//          if (a % 2 != 0) {
+//     document.write(a+"<br>");
+//        }
+//     }
+
+
+
+
+// for (var a = 1; a <= 100; a = a + 10) {
+
+//     for (var b = a; b < a + 10; b++) {
+//         document.write(b);
+//     }
+// document.write("<br>");
+// }
+
+
+
+
+
+
+
+
+
+
+
